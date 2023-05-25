805799c8d0ad280b12938a2cf41d100b03aca980
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2023-05-26 00:56:53 +0300

    add servey answers dashboard

---
+++ src/Components/SurveyAnswersDashboard/SurveyAnswersDashboard.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import SurveyCard from "../SurveyCard/SurveyCard";
+import SearchBar from "../SearchBar/SearchBar";
+import APIRoutes from '../../config.js';
+import './SurveyAnswersDashboard.css';
+
+
+
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+const SurveyAnswerDashboard = () => {
+  const [surveyAnswers, setSurveyAnswers] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    if (searchValue !== '') {
+      handleSearch();
+    } else {
+      fetchData();
+    }
+  }, [searchValue]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(APIRoutes.surveyAnswers);
+
+      setSurveyAnswers(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSearch = (value) => {
+    const endpoint = APIRoutes.search;
+    const queryString = `?term=${encodeURIComponent(value)}`;
+  
+    fetch(endpoint + queryString)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setSurveyAnswers(data);
+        console.log('Search results:', data);
+      })
+      .catch(error => {
+        console.error('Error performing search:', error);
+      });
+  };
+
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      <div className="dashboard-container">
+      {surveyAnswers.map((answer) => (
+          <SurveyCard key={uuidv4()} answer={answer} className="dashboard-card" />
+      ))}
+      </div>
+    </>
+
+  );
+};
+
+export default SurveyAnswerDashboard;
