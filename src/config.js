3f6fab18fbc7fffa820eeb5b8c7300277b62bfd4
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2023-05-26 01:08:38 +0300

    configure routes

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const APIRoutes = {
+    list: 'http://localhost:8000/api/list.json',
+    surveyAnswers: 'http://localhost:8000/api/answers',
+    search: 'http://localhost:8000/api/search',
+    aggregatedData: 'http://localhost:8000/api/XX2.json'
+  };
+  
+  export default APIRoutes;
