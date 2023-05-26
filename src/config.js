22ef021a34bf38e1843bdfff62b1be615c38ea29
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2023-05-26 12:16:29 +0300

    update the config file and change the url route

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const APIRoutes = {
-    list: 'http://localhost:8000/api/list.json',
-    surveyAnswers: 'http://localhost:8000/api/answers',
-    search: 'http://localhost:8000/api/search',
-    aggregatedData: 'http://localhost:8000/api/XX2.json'
+    list: 'http://localhost:8080/api/list.json',
+    surveyAnswers: 'http://localhost:8080/api/answers',
+    search: 'http://localhost:8080/api/search',
+    aggregatedData: 'http://localhost:8080/api/XX2.json'
   };
   
   export default APIRoutes;
